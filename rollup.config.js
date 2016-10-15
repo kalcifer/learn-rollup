420c778687e396dd70d88050fd4864f510ed9d11
Rahul Chanila <rahul@unbxd.com>
2016-10-15 17:51:38 +0530

    added node plugin and uglify plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,10 @@
+import process from 'process';
 import babel from 'rollup-plugin-babel';
 import eslint from 'rollup-plugin-eslint';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
+import uglify from 'rollup-plugin-uglify';
 
 export default {
   entry: 'src/scripts/main.js',
@@ -7,7 +12,22 @@ export default {
   format: 'iife',
   sourceMap: 'inline',
   plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true
+    }),
+    commonjs(),
     babel(),
-    eslint()
+    eslint({
+      exclude: [
+        'src/styles/**',
+      ]
+    }),
+    replace({
+      exclude: 'node_modules/**',
+      ENV: JSON.stringify(process.env.NODE_ENV || 'development')
+    }),
+    (process.env.NODE_ENV === 'production' && uglify())
   ]
 };
