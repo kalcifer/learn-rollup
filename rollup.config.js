102905f855085924505488f47eb37b4105986bb6
Rahul Chanila <rahul@unbxd.com>
2016-10-15 17:33:41 +0530

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,13 @@
+import babel from 'rollup-plugin-babel';
+import eslint from 'rollup-plugin-eslint';
+
+export default {
+  entry: 'src/scripts/main.js',
+  dest: 'build/js/main.min.js',
+  format: 'iife',
+  sourceMap: 'inline',
+  plugins: [
+    babel(),
+    eslint()
+  ]
+};
